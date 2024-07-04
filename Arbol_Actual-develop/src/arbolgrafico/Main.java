4c4c59624848ebca4a964ba48928e68968221c12
Ariana Sarango <166524037+ArianaSarango@users.noreply.github.com>
2024-07-04 08:56:38 -0500

    Add update files

---
+++ Arbol_Actual-develop/src/arbolgrafico/Main.java
@@ -0,0 +1,16 @@
+
+package arbolgrafico;
+
+/**
+ *
+ * @author Nikooll Camacho, Ivan Fernandez, José Riofrío, Ariana Sarango.
+ */
+public class Main {
+
+    
+    
+    public static void main(String[] args) {
+    
+        new Gui().setVisible(true);
+    }
+}
